fc610e12c430855e0b39c400df18f581c13e1503
CycraKumo <hikari.i.acg@gmail.com>
2024-06-15 16:29:19 +0900

    Modelの追加

---
+++ SoundAtlas/Models/VirtualInstrumentParameterModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoundAtlas.Models
+{
+    [Table("virtual_instrument_parameters")]
+    public class VirtualInstrumentParameterModel
+    {
+        [Key]
+        [Column("virtual_instrument_parameters_id")]
+        public int VirtualInstrumentParameterId { get; set; }
+
+        [Column("virtual_instruments_premsets_id")]
+        [Required]
+        public int VirtualInstrumentPresetId { get; set; }
+
+        [ForeignKey("VirtualInstrumentPresetId")]
+        public virtual VirtualInstrumentPresetModel VirtualInstrumentPreset { get; set; } = null!;
+
+        [Column("name")]
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; } = "";
+
+        [Column("value")]
+        [MaxLength(255)]
+        [Required]
+        public string Value { get; set; } = "";
+    }
+}
